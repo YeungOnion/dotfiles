6960c65671ed55c9a869e148d4af2c001e1e8921
Orion Yeung <11580988+orionyeung001@users.noreply.github.com>
2025-06-17 10:39:25 -0500

    fix(fish): don't export interactive shell config environment variables

--- home/private_dot_config/private_fish/executable_config.fish
+++ home/private_dot_config/private_fish/executable_config.fish
@@ -13,27 +13,27 @@ if status is-interactive
 
     # fzf related
     set fd_cmd (command -s fd || command -s fdfind)
-    set -gx FZF_DEFAULT_COMMAND $fd_cmd
-    set -gx FZF_DEFAULT_OPTS --ansi
+    set -g FZF_DEFAULT_COMMAND $fd_cmd
+    set -g FZF_DEFAULT_OPTS --ansi
 
     # pager related
     if command -q bat batcat
         # create soft link if not exist, overwrite otherwise
         ln --symbolic --force (command -s bat batcat)[-1] ~/.local/bin/bat
-        set -gx PAGER bat
-        set -gx MANPAGER "sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"
+        set -g PAGER bat
+        set -g MANPAGER "sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"
     else
         echo "cannot find `bat' pager, choosing `cat' instead" >&2
-        set -gx PAGER cat
+        set -g PAGER cat
     end
 
     # for tools I use directly and fisher funcs
-    set -gx sponge_purge_only_on_exit true
-    set -gx fzf_fd_opts --hidden --exclude="**/target/*" \
+    set -g sponge_purge_only_on_exit true
+    set -g fzf_fd_opts --hidden --exclude="**/target/*" \
         --exclude="**/build/*" --exclude="**/{.mypy,.ruff}_cache/*" \
         --exclude="**/.git/{objects,refs,logs}/*" \
         --exclude="**/.pixi/env/*"
-    set -gx EDITOR hx
+    set -g EDITOR hx
 
     if test -f ~/.asdf/asdf.fish
         source ~/.asdf/asdf.fish
@@ -43,18 +43,3 @@ if status is-interactive
     fish_add_path $RUSTUP_HOME/bin $CARGO_HOME/bin $GOPATH/bin $HOME/.local/bin $HOME/.pixi/bin
     source $__fish_config_dir/aliases.fish
 end
-
-if test -d $__fish_config_dir/venv_tooling.fish
-    source $__fish_config_dir/venv_tooling.fish
-end
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /home/orion/mambaforge/bin/conda
-    eval /home/orion/mambaforge/bin/conda "shell.fish" hook $argv | source
-end
-
-if test -f "/home/orion/mambaforge/etc/fish/conf.d/mamba.fish"
-    source "/home/orion/mambaforge/etc/fish/conf.d/mamba.fish"
-end
-# <<< conda initialize <<<
