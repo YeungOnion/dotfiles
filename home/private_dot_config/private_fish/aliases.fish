0caab660a9ac3ba3993e260259c3ceba677b4c2f
Orion Yeung <11580988+orionyeung001@users.noreply.github.com>
2026-01-08 21:47:20 -0600

    feat(fish): config and aliases

--- home/private_dot_config/private_fish/aliases.fish
+++ home/private_dot_config/private_fish/aliases.fish
@@ -1,14 +1,18 @@
 abbr --add jk --position command prevd
 abbr --add kj --position command nextd
-abbr --add cat --position command "$(command -s bat batcat)[-1] -p --color never"
 abbr --add grep --position command rg -uu # like grep -R
+abbr --add find --position command fd -IH
 abbr --add rm --position command trash # try to use trash pls
 abbr --add g --position command git
 abbr --add gco --position command git checkout
 abbr --add sg --position command ast-grep
+abbr --add tree --position command tree --gitignore
+abbr --add glow --position command glow --pager
 
 alias du 'du -h'
 alias mkdir 'mkdir -p'
+alias tmux 'TERM=xterm-256color command tmux'
+alias glow 'glow --width 120'
 
 if command -q git-branchless
     alias git 'git-branchless wrap -- '
@@ -32,8 +36,7 @@ alias mv 'mv -i'
 abbr --add sl --position command ls
 
 # utils
-abbr .4dir --set-cursor="<+++>" "$(string join \n -- 'for dir i n */' 'pushd $dir' '<+++>' 'popd' 'end')"
-
+abbr .4dir --set-cursor="<+++>" "$(string join \n -- 'for dir in */' 'pushd $dir' '<+++>' 'popd' 'end')"
 
 # tar is a monster
 abbr --add targzip --position command 'tar --create --gzip --verbose --file'
--- home/private_dot_config/private_fish/executable_config.fish
+++ home/private_dot_config/private_fish/executable_config.fish
@@ -15,25 +15,26 @@ if status is-interactive
     set fd_cmd (command -s fd || command -s fdfind)
     set -g FZF_DEFAULT_COMMAND $fd_cmd
     set -g FZF_DEFAULT_OPTS --ansi
+    fzf_configure_bindings --directory='super-p'
 
     # pager related
-    if command -q bat batcat
+    if command -s bat batcat
         # create soft link if not exist, overwrite otherwise
         ln --symbolic --force (command -s bat batcat)[-1] ~/.local/bin/bat
-        set -g PAGER bat
-        set -g MANPAGER "sh -c 'sed -u -e \"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\" | bat -p -lman'"
+        set -gx PAGER bat -p
+        set -g MANPAGER batman
     else
         echo "cannot find `bat' pager, choosing `cat' instead" >&2
-        set -g PAGER cat
+        set -gx PAGER cat
     end
 
     # for tools I use directly and fisher funcs
     set -g sponge_purge_only_on_exit true
-    set -g fzf_fd_opts --hidden --exclude="**/target/*" \
+    set -gx fzf_fd_opts --hidden --exclude="**/target/*" \
         --exclude="**/build/*" --exclude="**/{.mypy,.ruff}_cache/*" \
         --exclude="**/.git/{objects,refs,logs}/*" \
         --exclude="**/.pixi/env/*"
-    set -g EDITOR hx
+    set -gx EDITOR hx
 
     if test -f ~/.asdf/asdf.fish
         source ~/.asdf/asdf.fish
@@ -42,4 +43,13 @@ if status is-interactive
 
     fish_add_path $RUSTUP_HOME/bin $CARGO_HOME/bin $GOPATH/bin $HOME/.local/bin $HOME/.pixi/bin
     source $__fish_config_dir/aliases.fish
+
+    # projects
+    direnv hook fish | source
+    mise activate fish | source
+end
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/onion/packages/google-cloud-sdk/path.fish.inc' ]
+    . '/Users/onion/packages/google-cloud-sdk/path.fish.inc'
 end
