a7e8ce1f3b79a2a544866a962a541d6aea73ed6e
Orion Yeung <11580988+orionyeung001@users.noreply.github.com>
2026-05-26 16:29:24 -0500

    feat(fzf): keybinds for pipe through fzf

--- home/private_dot_config/private_fish/functions/__append_pipe_fzf.fish
+++ home/private_dot_config/private_fish/functions/__append_pipe_fzf.fish
@@ -1,19 +1,20 @@
 function __append_pipe_fzf
     switch (commandline --is-valid)
-    case 2
-        echo -e "\nIncomplete command, cannot page its output"
-    case 1
-        echo -e "\nErroneous command, cannot page its output"
-    case '*'
-        set -l cmd2fzf ''
-        commandline | read -at cmd2fzf
+        case 2
+            echo -e "\nIncomplete command, cannot page its output"
+        case 1
+            echo -e "\nErroneous command, cannot page its output"
+        case '*'
+            set -f cmd2fzf ''
+            commandline | read -at cmd2fzf
 
-        set -l fzf_selections (echo ($cmd2fzf | _fzf_wrapper --multi --ansi --height=60%))
-        if test -n $fzf_selections
-            echo $fzf_selections
-        else
-            echo 'exited with error, fzf_selections not set'
-        end
+            set -f op_view 'bat {}'
+            set -f op_edit 'hx {}'
+            set -f op_cp 'echo {} | fish_clipboard_copy'
+
+            _fzf_wrapper --multi --ansi --height=60% \
+                --bind "start:reload:$cmd2fzf" \
+                --bind "f1:become($op_view),ctrl-e:become($op_edit),ctrl-y:execute-silent($op_cp)+abort"
     end
 
     commandline --function repaint
